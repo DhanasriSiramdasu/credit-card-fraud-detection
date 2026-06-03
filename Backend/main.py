545a5562248808eb40c421502f1638ec5a6f5d96
DhanasriSiramdasu <siramdasudhanasri@gmail.com>
2026-06-03 12:00:09 +0530

    Update CORS for deployment

--- Backend/main.py
+++ Backend/main.py
@@ -15,7 +15,8 @@ scaler = joblib.load(os.path.join(BASE_DIR, './models/scaler.pkl'))
 app=FastAPI(title="Fraud Detection API")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+                   "https://creditcard-fraud-detection-ml-1.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
